341068b693f02ff574feb738d4d77b15621f122b
Marina Barinova <marina.alive888@yandex.ru>
2023-02-18 19:58:28 +0300

    Задача2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 36. задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечетных позициях
+//[3,7,23,12]->19
+//[-4,-6,89,6]->0
+
+
+int []  CreatNewArray (int size, int minVal, int maxVal)
+{
+    int [] array = new int [size];
+    for(int i = 0; i <size; i++)
+    {
+        array [i] = new Random().Next(minVal,maxVal+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+ void OddInd (int [] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        
+        if (i % 2 == 0)
+        {
+            sum = sum +0;
+        }
+        else
+        {
+            sum = sum + array[i];
+        }
+    }
+    Console.WriteLine($"-> {sum} odd");
+}
+int [] Array = CreatNewArray(5,-99,99);
+
+ShowArray(Array);
+OddInd(Array);
